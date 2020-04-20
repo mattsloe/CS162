daf8c4d23dceeed8f24076090ee84d860eb6a99c
mattsloe <56812854+mattsloe@users.noreply.github.com>
2020-04-19 21:41:34 -0700

    Implemented functions for lab3. Tested in debugger on windows. Set up project in visual studio

--- labs/lab3/list.cpp
+++ labs/lab3/list.cpp
@@ -4,14 +4,33 @@ using namespace std;
 
 bool insert(int position, int val, int intList[], int& size)
 {
-	//insert your code here and modify the following line
-	return true;
+	//inserts the value
+	if (position < 100) {
+		if (position >= size) size = position+1; //change the array size if the position is greater than the array size
+		intList[position] = val; //put val into the intList at the right index
+		return true;
+	}
+	else return false;
 }
 
 bool remove(int position, int& val, int intList[], int& size)
 {
 	//insert your code here and modify the following line
-	return true;
+	//make sure position is valid
+	if (position < size) {
+
+		//store value to be deleted to val
+		val = intList[position];
+		//move the other values up by one
+		for(int i = position; i < size-1; i++)
+		{
+			intList[position] = intList[position + 1];
+		}
+		//decrement size
+		size--;
+		return true;
+	}
+	else return false;
 }
 
 void print(const int intList[], int size)
