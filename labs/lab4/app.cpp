b5fe6e45aea64538fde52ffd772a6ba94f5a17fb
mattsloe <mattsloe@Matt-PC.localdomain>
2020-04-28 13:44:21 -0700

    Added Instructor Files for Lab 4

---
+++ labs/lab4/app.cpp
@@ -0,0 +1,16 @@
+#include "student.h"
+
+int main()
+{
+	Student s1("G10", 3.9);
+	Student s2("G20", 3.5);
+
+	s1.print();
+	s2.print();
+
+	//write code to test Student::isLessThanByID
+	
+	//write code to test Student::qualifyForHonor
+
+	return 0;
+}
---
+++ labs/lab4/student.cpp
@@ -0,0 +1,8 @@
+#include "student.h"
+
+//implement the required 3 functions here
+
+void Student::print() const
+{
+	cout << id << '\t' << gpa << endl;
+}
