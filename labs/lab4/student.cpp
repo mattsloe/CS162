f6d52453bf82aa4a7f69cdb2a965b0cfa7409144
mattsloe <mattsloe@Matt-PC.localdomain>
2020-04-28 23:21:02 -0700

    Did the assignment. Created the submit file.

--- labs/lab4/student.cpp
+++ labs/lab4/student.cpp
@@ -1,6 +1,23 @@
 #include "student.h"
 
 //implement the required 3 functions here
+Student::Student(const char initId[], double gpa)
+{
+	Student::gpa = gpa;
+	strcpy(Student::id,initId);
+}
+
+bool Student::isLessThanByID(const Student& aStudent) const
+{
+	if(strcmp(id,aStudent.id) < 0) return true;
+	return false;
+}
+
+bool Student::qualifyForHonor(double minGpaForHonor) const
+{
+	if(gpa >= minGpaForHonor) return true;
+	return false;
+}
 
 void Student::print() const
 {
--- labs/lab4/student.h
+++ labs/lab4/student.h
@@ -2,15 +2,28 @@
 #define STUDENT_H
 
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class Student
 {
 public:
+
+	//Initialized constructor for Student object
+	//POSTCONDITION: initID = id, gpa = gpa
 	Student(const char initId[], double gpa);
+
+	//function that compares the calling student to the referenced student aStudent
+	//POSTCONDITION: returns true if the calling Student is less than aStudent
 	bool isLessThanByID(const Student& aStudent) const;
+
+	//function that checks if the Student qualifies for honors based on minGpaForHonor
+	//POSTCONDTION: returns true if minGpaForHonor <= gpa
 	bool qualifyForHonor(double minGpaForHonor) const;
+
+	//function that displays the student object to stdout
 	void print()const;
+
 private:
 	const static int MAX_CHAR = 100;
 	char 	id[MAX_CHAR];
