fb534f70949071f8fb7a27fe13776df47fec34de
mattsloe <mattsloe@Matt-PC.localdomain>
2020-04-30 11:52:38 -0700

    Added midterm practice files and got rid of others

---
+++ midtermPractice/array.cpp
@@ -0,0 +1,5 @@
+//array.cpp 
+
+#include "array.h"
+
+//put the implementations of your assigned functions here
---
+++ midtermPractice/main.cpp
@@ -0,0 +1,17 @@
+#include "array.h"
+using namespace std;
+
+const int CAP = 100;
+int main()
+{
+	int list[CAP];
+	int size = 10;
+    build(list, size);
+    display(list, size);
+
+    //PLEASE PUT YOUR CODE HERE to call the function assigned
+
+    display(list, size);
+    
+    return 0;
+}
